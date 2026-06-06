3c05b63e50d85051cf8a76c1e511c8d5811160fd
Alexey Slovetskikh <alexslcraft@gmail.com>
2026-06-07 01:36:53 +0300

    добавлен копирайт в футер

--- src/components/layouts/AppFooter.vue
+++ src/components/layouts/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="flex h-40 bg-[#f8fdef] dark:bg-[#0a1120]">
-
+    <footer class="flex h-40 bg-[#f8fdef] dark:bg-[#0a1120] rounded-2xl justify-center items-center">
+        <p class="dark:text-white font-semibold md:text-xl">© 2026 Leanly. Все права защищены.</p>
     </footer>
 </template>
