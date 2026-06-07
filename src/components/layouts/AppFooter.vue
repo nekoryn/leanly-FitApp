cf6f27122d8f67c1b8f5a31a4626f2f0b9ebdfa7
Alexey Slovetskikh <alexslcraft@gmail.com>
2026-06-07 05:10:33 +0300

    fix отображения ссылки в футере на телефонах

--- src/components/layouts/AppFooter.vue
+++ src/components/layouts/AppFooter.vue
@@ -4,7 +4,7 @@
 </script>
 
 <template>
-    <footer class="flex h-40 bg-[#f8fdef] dark:bg-[#0a1120] rounded-2xl justify-center items-center gap-5">
+    <footer class="flex flex-col h-40 bg-[#f8fdef] dark:bg-[#0a1120] rounded-2xl justify-center items-center gap-5 md:flex-row">
         <p class="dark:text-white font-semibold md:text-xl">© 2026 Leanly. Все права защищены.</p>
         <a href="https://github.com/zularx/Leanly/issues/new" class="dark:text-[#3abdf7] text-[#419400] dark:hover:text-[#59ccfe] hover:text-[#53be00] dark:active:text-[#0ab4fc] active:text-[#4bae00]">Предложить улучшение проекта.</a>
     </footer>
