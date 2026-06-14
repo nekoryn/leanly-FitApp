bb0cc0f8abcca33bd10be2d9a16565b899f90322
Alexey Slovetskikh <alexslcraft@gmail.com>
2026-06-14 06:36:23 +0300

    исправление бага при котором на планшете почему то после футтера было место

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -7,7 +7,7 @@
     <AppHeader>
 
     </AppHeader>
-    <main class="flex flex-col items-center justify-center mt-40 mb-40 md:mt-60 md:mb-60">
+    <main class="flex flex-col items-center min-h-dvh justify-center mt-40 mb-40 md:mt-60 md:mb-60">
         <slot></slot>
     </main>
     <AppFooter>
