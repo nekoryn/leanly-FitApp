f4a8145c736cc9c90fcb8c67b396cca5e1f8e41e
Alexey Slovetskikh <alexslcraft@gmail.com>
2026-06-14 06:48:34 +0300

    исправление бага при котором на планшете почему то после футтера было место 2

--- src/layout/DefaultLayout.vue
+++ src/layout/DefaultLayout.vue
@@ -4,13 +4,15 @@
 </script>
 
 <template>
-    <AppHeader>
-
-    </AppHeader>
-    <main class="flex flex-col items-center min-h-dvh justify-center mt-40 mb-40 md:mt-60 md:mb-60">
-        <slot></slot>
-    </main>
-    <AppFooter>
-        
-    </AppFooter>
+    <div class="min-h-dvh grid grid-rows-[auto_1fr_auto] bg-[#e1e6d9] dark:bg-[#0a1120]">
+        <AppHeader>
+    
+        </AppHeader>
+        <main class="flex flex-col items-center justify-center mt-40 mb-40 md:mt-60 md:mb-60">
+            <slot></slot>
+        </main>
+        <AppFooter>
+            
+        </AppFooter>
+    </div>
 </template>
